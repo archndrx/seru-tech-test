0dddc8fd3256c5613186d013a95d9b67c6e14c66
Chandra <ardhyachandrax@gmail.com>
2024-01-31 20:27:28 +0700

    feat: create screen one

---
+++ lib/view/screen_one.dart
@@ -0,0 +1,159 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:seru_tech_test/provider/screen_one_provider.dart';
+import 'package:seru_tech_test/shared/theme.dart';
+import 'package:seru_tech_test/widgets/buttons.dart';
+import 'package:seru_tech_test/widgets/form.dart';
+
+class ScreenOne extends StatefulWidget {
+  const ScreenOne({Key? key}) : super(key: key);
+
+  @override
+  State<ScreenOne> createState() => _ScreenOneState();
+}
+
+class _ScreenOneState extends State<ScreenOne> {
+  String _selectedProvince = '';
+  String _selectedDistrict = '';
+  String _selectedSubDistrict = '';
+  String _selectedVillage = '';
+
+  @override
+  void initState() {
+    super.initState();
+    Provider.of<ScreenOneProvider>(context, listen: false).fetchProvinces();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<ScreenOneProvider>(builder: (context, provider, _) {
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text("Screen 1"),
+        ),
+        body: ListView(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 24,
+          ),
+          children: [
+            const SizedBox(
+              height: 30,
+            ),
+            Container(
+              padding: const EdgeInsets.all(22),
+              decoration: BoxDecoration(
+                color: lightBackgroundColor,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Column(
+                children: [
+                  const CustomFormField(title: 'First Name'),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  const CustomFormField(title: 'Last Name'),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  const CustomFormField(
+                    title: 'Biodata',
+                    keyType: TextInputType.multiline,
+                    minLines: 6,
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  CustomDropDownSearchForm(
+                    title: 'Provinsi',
+                    items: provider.provinceNames,
+                    selectedItem: _selectedProvince,
+                    onChanged: (value) {
+                      setState(() {
+                        _selectedProvince = value.toString();
+                        int? provinceId = provider.provinceData.firstWhere(
+                          (province) => province['name'] == _selectedProvince,
+                        )['id'];
+                        if (provinceId != null) {
+                          Provider.of<ScreenOneProvider>(context, listen: false)
+                              .fetchDistricts(provinceId);
+                        }
+                      });
+                    },
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  CustomDropDownSearchForm(
+                    title: 'Kabupaten/Kota',
+                    items: provider.districtNames,
+                    selectedItem: _selectedDistrict,
+                    onChanged: (value) {
+                      setState(
+                        () {
+                          _selectedDistrict = value.toString();
+                          int? districtId = provider.districtData.firstWhere(
+                            (district) => district['name'] == _selectedDistrict,
+                          )['id'];
+                          if (districtId != null) {
+                            Provider.of<ScreenOneProvider>(context,
+                                    listen: false)
+                                .fetchSubdistricts(districtId);
+                          }
+                        },
+                      );
+                    },
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  CustomDropDownSearchForm(
+                    title: 'Kecamatan',
+                    items: provider.subdistrictNames,
+                    selectedItem: _selectedSubDistrict,
+                    onChanged: (value) {
+                      setState(() {
+                        _selectedSubDistrict = value.toString();
+                        int? subDistrictId =
+                            provider.subdistrictData.firstWhere(
+                          (subdistrictData) =>
+                              subdistrictData['name'] == _selectedSubDistrict,
+                        )['id'];
+                        if (subDistrictId != null) {
+                          Provider.of<ScreenOneProvider>(context, listen: false)
+                              .fetchVillages(subDistrictId);
+                        }
+                      });
+                    },
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  CustomDropDownSearchForm(
+                    title: 'Kelurahan',
+                    items: provider.villageNames,
+                    selectedItem: _selectedVillage,
+                    onChanged: (value) {
+                      setState(() {
+                        _selectedVillage = value.toString();
+                      });
+                    },
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            CustomFilledButton(
+              title: 'Continue',
+              onPressed: () {},
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+          ],
+        ),
+      );
+    });
+  }
+}
