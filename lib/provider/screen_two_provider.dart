6084402c01fdf7218dd004cd758ba01e06b1f9cb
Chandra <ardhyachandrax@gmail.com>
2024-02-01 13:39:10 +0700

    feat: add function

--- lib/provider/screen_two_provider.dart
+++ lib/provider/screen_two_provider.dart
@@ -1,11 +1,15 @@
 import 'dart:io';
-
+import 'package:flutter_tesseract_ocr/android_ios.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ScreenTwoProvider extends ChangeNotifier {
+  String _nik = '';
   File? selfieImage;
   File? ktpImage;
   File? freeImage;
+
+  String get nik => _nik;
 
   void setImage(File? image, String type) {
     switch (type) {
@@ -35,5 +39,46 @@ class ScreenTwoProvider extends ChangeNotifier {
         break;
     }
     notifyListeners();
+  }
+
+  Future<void> getImage(ImageSource source, String type) async {
+    final pickedFile = await ImagePicker().pickImage(source: source);
+
+    if (pickedFile != null) {
+      File imageFile = File(pickedFile.path);
+      setImage(imageFile, type);
+
+      // Read text from KTP after setting the image
+      if (type == 'ktp') {
+        await readTextFromKtp(imageFile);
+      }
+    }
+  }
+
+  Future<void> readTextFromKtp(File imageFile) async {
+    try {
+      String result = await FlutterTesseractOcr.extractText(imageFile.path);
+
+      // Proses untuk mengekstrak NIK dari teks yang sudah ada
+      String nikCleaned = extractNikFromText(result);
+
+      if (nikCleaned.isNotEmpty) {
+        _nik = nikCleaned;
+      } else {}
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  String extractNikFromText(String text) {
+    // Mencari pola NIK yang terdiri dari 16 digit
+    RegExp nikRegExp = RegExp(r'\b\d{16}\b');
+    Match? nikMatch = nikRegExp.firstMatch(text);
+
+    if (nikMatch != null) {
+      return nikMatch.group(0) ?? ''; // Mengambil nilai yang cocok
+    } else {
+      return '';
+    }
   }
 }
